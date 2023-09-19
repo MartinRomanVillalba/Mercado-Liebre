b86ffc8e1c0f0905ed148df7b7870c53cb013f70
MartinRomanVillalba <romantincho@gmail.com>
2023-09-19 19:58:21 -0300

    Cambios en app y package.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -18,5 +18,5 @@ app.get("/register", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Escuchando en el puerto ${port}`);
+    console.log(`Servidor iniciado en http://localhost:${port}`);
 });
