48d42e93252d179e946ac06c6c4098efb4d16bb2
MartinRomanVillalba <romantincho@gmail.com>
2023-09-19 20:32:12 -0300

    ahora si terminado

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/src/public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html") );
+    res.sendFile(path.join(__dirname, "/src/views/home.html") );
 });
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/login.html") );
+    res.sendFile(path.join(__dirname, "/src/views/login.html") );
 });
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/register.html") );
+    res.sendFile(path.join(__dirname, "/src/views/register.html") );
 });
 
 app.listen(port, () => {
